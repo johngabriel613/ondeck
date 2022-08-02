714d1adf41e611e424490187d842e6b6d51fa5d2
John Paderog <91768994+johngabriel613@users.noreply.github.com>
2022-08-02 23:23:18 +0800

    update

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const openNav = document.querySelector('.menu img')
-const closeNav = document.querySelector('.products-label img')
-const productsNav = document. querySelector('.products-wrapper')
+const openNav = document.querySelector('.menu')
+const closeNav = document.querySelector('.product-label img')
+const productNav = document.querySelector('.products-wrapper')
 
 openNav.addEventListener('click', () => {
-    productsNav.classList.add('active')
+    productNav.classList.add('active')
 })
 
 closeNav.addEventListener('click', () => {
-    productsNav.classList.remove('active')
+    productNav.classList.remove('active')
 })
